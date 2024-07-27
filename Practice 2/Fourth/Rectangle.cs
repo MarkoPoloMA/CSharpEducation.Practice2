41f294afc96d86859b2eecd8975152a77f6cf42c
MarkoPoloMA <mark.utrobin@mail.ru>
2024-07-27 17:00:50 +0400

    Commit#5

--- Practice 2/Fourth/Rectangle.cs
+++ Practice 2/Fourth/Rectangle.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿//using System;
 
-namespace Practice_2
-{
-    class ProgramThird
-    {
-        static void Main()
-        {
-            var test = new RecA();
-            var test1 = new RecB();
-            test1.RectangleB();
-            test.RectangleA();
-            Console.ReadKey();
-        }
-    }
-}
+//namespace Practice_2
+//{
+//    class ProgramThird
+//    {
+//        static void Main()
+//        {
+//            var test = new RecA();
+//            var test1 = new RecB();
+//            test1.RectangleB();
+//            test.RectangleA();
+//            Console.ReadKey();
+//        }
+//    }
+//}
