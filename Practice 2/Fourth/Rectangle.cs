b78d9aa03c858566f650215bed499404e7ef25f7
MarkoPoloMA <mark.utrobin@mail.ru>
2024-07-27 16:35:38 +0400

    Четвертая задача

---
+++ Practice 2/Fourth/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practice_2
+{
+    class ProgramThird
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите высоту треугольника: ");
+            int heigth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите элемент чем заполнять: ");
+            char elem = char.Parse(Console.ReadLine());
+
+            for (var i = 1; i <= heigth; i++)
+            {
+                string output = "";
+                for (var j = 0; j < i; j++)
+                {
+                    output += elem.ToString();
+                }
+                Console.WriteLine(output);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
